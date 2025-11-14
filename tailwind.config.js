085b0b728acb0008512cc0d39fc93e99c6330aa9
alifirdaus-0 <firdausali746@gmail.com>
2025-11-14 21:06:51 +0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],  // <-- pastikan ini menunjuk ke folder src
+  theme: { extend: {} },
+  plugins: [],
+};
