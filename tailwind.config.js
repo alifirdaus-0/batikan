510f0869af6f1011f4b74cf3fd55a668ff20ec79
alifirdaus-0 <firdausali746@gmail.com>
2025-11-21 20:24:17 +0700

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],  // <-- pastikan ini menunjuk ke folder src
-  theme: { extend: {} },
-  plugins: [],
-};
